7de6d47ec82bd7a14feb48fc62ecc3bece50e54e
Sonu <thesonukumar357@gmail.com>
2026-03-26 13:12:16 +0530

    new Changes

--- backend/routes/productCategory.routes.js
+++ backend/routes/productCategory.routes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 const router = express.Router();
-import { getAll, getOne, create, update, remove } from '../controllers/productCategory.controller.js';
+import { getAll, getOne, create, update, remove , getOneById  } from '../controllers/productCategory.controller.js';
 import { protect } from '../middleware/auth.middleware.js';
 import upload from '../middleware/upload.middleware.js';
 
 router.get("/", getAll);
 router.get("/:slug", getOne);
+router.get("/single/:id", getOneById);
+
 router.post("/", protect, upload.single("image"), create);
 router.put("/:id", protect, upload.single("image"), update);
 router.delete("/:id", protect, remove);
